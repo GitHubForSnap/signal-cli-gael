5cd8575289123d9eafd729f4f4597089bf23ca9f
Admin Build <admin_build@build.legoff.be>
2025-09-18 16:33:11 +0200

    Update Gradle to v9.1.0

--- snap/local/build/build.sh
+++ snap/local/build/build.sh
@@ -10,8 +10,7 @@ rm -f gradle graalvm-jdk native-image.properties signal-cli
 
 # Set up versions
 SIGNAL_CLI_VERSION="v0.13.19"
-#GRADLE_VERSION="8.14.3"
-GRADLE_VERSION="9.0.0"
+GRADLE_VERSION="9.1.0"
 GRAALVM_VERSION="21"
 
 # Update the container & install the required tools
